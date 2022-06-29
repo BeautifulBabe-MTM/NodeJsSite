9e35eaeef9678689fe9c8bb4b7b738fc51b4cb7d
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2022-06-29 23:02:29 +0300

    added admin panel and db of houses

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const { request } = require('express');
-const express = require('express');
-const mongoose = require('mongoose');
-const exphbs = require('express-handlebars');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(todoRoutes);
+app.use(todoRoutes)
 
-async function start(){
-    try{
-        await mongoose.connect('mongodb+srv://yaroslave:123zxc34@cluster0.yxkbygz.mongodb.net/todos', {
-            useNewUrlParser: true})
-
+async function start() {
+  try {
+    await mongoose.connect('mongodb+srv://yaroslave:123zxc34@cluster0.yxkbygz.mongodb.net/todos', {
+            useNewUrlParser: true}
+        )
         app.listen(PORT, () => {
-            console.log('Server has been started!')
+          console.log('Server has been started...')
         })
-    }
-    catch(e){
+      } catch (e) {
         console.log(e)
+      }
     }
-}
-
-start()
+    
+    start()
